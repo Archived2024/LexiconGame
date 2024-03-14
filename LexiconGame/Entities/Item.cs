0954464b29b49fea5fb14d892a47ccb8e8e1d3b8
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-03-14 14:04:38 +0100

    Add Item

---
+++ LexiconGame/Entities/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconGame.Entities
+{
+    internal class Item
+    {
+        private readonly string name;
+        public ConsoleColor Color { get; }
+        public string Symbol { get; }
+
+        public Item(string symbol, ConsoleColor color, string name)
+        {
+            Symbol = symbol;
+            Color = color;
+            this.name = name;
+        }
+
+        public override string ToString() => name;
+
+    }
+}
