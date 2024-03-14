6e96e85ecc40a607da74bc40d3d0ef964999fdc4
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-03-14 13:11:20 +0100

    Add Add

--- LexiconGame.LinitedList/LimitedList.cs
+++ LexiconGame.LinitedList/LimitedList.cs
@@ -5,9 +5,21 @@
         private readonly int capacity;
         private List<T> list;
 
+        public int Count => list.Count;
+        public bool IsFull => capacity <= Count;
+
         public LimitedList(int capacity)
         {
             this.capacity = Math.Max(capacity, 2);
+            list = new List<T>(this.capacity);
+        }
+
+        public bool Add(T item)
+        {
+            ArgumentNullException.ThrowIfNull(item, nameof(item));
+
+            if (IsFull) return false;
+            list.Add(item); return true;
         }
     }
 }
