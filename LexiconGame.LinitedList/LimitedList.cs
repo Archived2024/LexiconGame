71f675f8b252f5dfd5da52eeb1f58d72b301c0a8
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-03-14 11:44:06 +0100

    Add LimitedList

---
+++ LexiconGame.LinitedList/LimitedList.cs
@@ -0,0 +1,13 @@
+﻿namespace LexiconGame.LinitedList
+{
+    public class LimitedList<T>
+    {
+        private readonly int capacity;
+        private List<T> list;
+
+        public LimitedList(int capacity)
+        {
+            this.capacity = Math.Max(capacity, 2);
+        }
+    }
+}
