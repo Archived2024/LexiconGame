942a37759c1c7f74a0b02b8fefe01da9ca80058b
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-03-19 14:58:32 +0100

    Add appsettings.json

--- LexiconGame/Program.cs
+++ LexiconGame/Program.cs
@@ -1,4 +1,13 @@
-﻿var game = new Game(new ConsoleUI(), new Map(width: 10, height: 10));
+﻿using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+                            .SetBasePath(Environment.CurrentDirectory)
+                            .AddJsonFile("appsettings.json", false, reloadOnChange: true)
+                            .Build();
+
+var test = config.GetSection("game:ui").Value;
+
+var game = new Game(new ConsoleUI(), new Map(width: 10, height: 10));
 game.Run();
 
 Console.WriteLine("Game over!");
